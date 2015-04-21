8596e8001e96905ccdabc7fe80fa5d63201d00dc
dave-cz <david.charvat.cz@gmail.com>
2015-04-21 12:18:34 +0200

    rewrite into object aukroAlarm

--- aukroalarm.user.js
+++ aukroalarm.user.js
@@ -9,209 +9,121 @@
 // @updateURL     https://raw.githubusercontent.com/micovo/aukroalarmscript/master/aukroalarm.user.js
 // @include       http://aukro.cz/*
 // @require       http://code.jquery.com/jquery-latest.min.js
+// @require       https://raw.githubusercontent.com/bunkat/later/master/later.min.js
 // @run-at        document-end
 // ==/UserScript==
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
+// contributors
+//   dave-cz
 
-//Timers setup
-AlarmTime = 5*1000; //miliseconds
-RefreshTime = 60*1000; //miliseconds
+var aukroAlarm = {
+    alarmTime: 5*1000, // miliseconds
+    refreshTime: 60*1000, // miliseconds
+    secondsToEndAlarm: 3*60, // seconds
+    mAlarmSound: new Audio('https://www.dropbox.com/s/opz32tj8hzzxqi1/smokealarm.mp3?dl=1'),
+    watching: false,
+    secondsToEnd: -1
+};
 
-//Alarm setup
-secondsToEndAlarm = 3*60; //seconds
-
-//Link on the alarm sample
-mAlarmSound = new Audio("https://www.dropbox.com/s/opz32tj8hzzxqi1/smokealarm.mp3?dl=1");
- 
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-
-//Global variables
-var sledovano = 0;
-var auctionId = window.location.href.split('.html')[0].slice(-10); // auction ID from page URL
-var secondsToEnd = -1;
-
-
-//////////////////////////////////////////////////////////////////////
-//
-//  Main
-//
-//////////////////////////////////////////////////////////////////////
-
-$(document).ready(function() 
-{  
-    var timeStr = $("li.timeInfo strong").text();
-    
-    secondsToEnd = parseInt(timeStr);
-    
-    if (timeStr.indexOf("Méně než minuta") > -1)
-    {
-        secondsToEnd = 1;
-    }
-    else if (timeStr.indexOf("minut") > -1)
-    {
-        secondsToEnd = secondsToEnd * 60;
-    }
-    else if (timeStr.indexOf("hodin") > -1)
-    {
-        secondsToEnd = secondsToEnd * 60 * 60;
-    }
-    else if ((timeStr.indexOf("den") > -1) || (timeStr.indexOf("dn") > -1))
-    {
-        secondsToEnd = secondsToEnd * 24 * 60 * 60; 
-    }
-    else
-    {
-        secondsToEnd = 0;
-    }
-    
-    if (secondsToEnd < 24*60*60)
-    {
-        secondsToEnd = UpdateSecondsToEnd();
-    }
-    
-
-    sledovano = getGMCookie(auctionId, 0);
-    
-    if (sledovano == 1)
-    {
-        $('div#siBidForm2').append('<div id="micaContainer" style="padding:20px;"><button id="micaButton">Zrušit sledování</button>&nbsp;&nbsp;&nbsp;Do konce: <span id="secondsToEnd">'+secondsToEnd+'</span> s, Alarm: '+secondsToEndAlarm+' s </div>');
-    }
-    else
-    {
-        $('div#siBidForm2').append('<div id="micaContainer" style="padding:20px;"><button id="micaButton">Sledovat skriptem</button>&nbsp;&nbsp;&nbsp;Do konce: <span id="secondsToEnd">'+secondsToEnd+'</span> s</div>');
-    }
-    
-    
-   
-    
-    SecondTickTimer();
-});
-
-
-
-
-//////////////////////////////////////////////////////////////////////
-//
-//  Events
-//
-//////////////////////////////////////////////////////////////////////
-
-$(document).on( "click", "button#micaButton", function()
-{
-    
-    if (sledovano == 1)
-    {
-        setGMCookie(auctionId, 0);
-    }
-    else
-    {
-        setGMCookie(auctionId, 1);
-        
-    }
-    
-    location.reload();
-});
-
-//////////////////////////////////////////////////////////////////////
-//
-//  Timers
-//
-//////////////////////////////////////////////////////////////////////
-
-
-function AlarmTimer()
-{
-    mAlarmSound.play();  
-    window.setTimeout(AlarmTimer, AlarmTime);
-}
-
-
-function RefreshTimer()
-{
-    location.reload();
-    window.setTimeout(RefreshTimer, RefreshTime);
-}
-
-function SecondTickTimer()
-{
-    secondsToEnd = UpdateSecondsToEnd();
-    $("span#secondsToEnd").text(secondsToEnd);
-    
-    if (sledovano == 1)
-    {
-        if (secondsToEnd > 0)
-        {
-            if (secondsToEnd <= secondsToEndAlarm)
-            {
-                AlarmTimer();
-            }
-        }
-    }
-    
-    window.setTimeout(SecondTickTimer, 1000);
-}
-
-
-//////////////////////////////////////////////////////////////////////
-//
 //  Functions
-//
-//////////////////////////////////////////////////////////////////////
 
-function UpdateSecondsToEnd()
-{
-    timeStr = $("li.timeInfo").text().split(",")[2];
-    timeStr = timeStr.substring(1, 9);
-    var endDate = parseTime(timeStr);
-    var nowDate = new Date();
-
-    if (nowDate > endDate)
-    {
+aukroAlarm.parseSecondsToEnd = function() {
+    var toEndStr = $('li.timeInfo strong').text();
+    if (toEndStr.indexOf('den') > -1 || toEndStr.indexOf('dn') > -1) {
+        return 24 * 60 * 60;
+    }
+    var timeStr = $('li.timeInfo').text().split(',')[2].substring(1, 9),
+        endDate = this.parseTime(timeStr),
+        nowDate = new Date();
+    if (nowDate > endDate) {
         endDate = endDate.setDate(endDate.getDate()+1);   
     }
-
     return Math.round((endDate - nowDate)/1000);
-}
+};
 
-//////////////////////////////////////////////////////////////////////
-//                                  
-//  Helpers
-//
-//////////////////////////////////////////////////////////////////////
+aukroAlarm.setGMCookie = function (key, value) {
+    GM_setValue(key, value);
+};
 
+aukroAlarm.getGMCookie = function (key, defaultValue) {
+    var gotValue;
+    gotValue = GM_getValue(key, null);
+    if(gotValue == null){
+        this.setGMCookie(key, defaultValue);
+        gotValue = defaultValue;
+    }
+    return gotValue;
+};
 
-function parseTime(timeStr, dt) {
+aukroAlarm.parseTime = function (timeStr, dt) {
     if (!dt) {
         dt = new Date();
     }
- 
-    var time = timeStr.split(":");
- 
+    var time = timeStr.split(':');
     dt.setHours(parseInt(time[0]));
     dt.setMinutes(parseInt(time[1]));
     dt.setSeconds(parseInt(time[2]));
     return dt;
+};
+
+aukroAlarm.isInt = function (n) {
+    return (parseFloat(n) == parseInt(n, 10) && !isNaN(n));
 }
 
-function setGMCookie(key, value){
-	GM_setValue(key, value);
-}
+//  Worker
 
-function getGMCookie(key, defaultValue)
-{
-	var gotValue;
-	gotValue = GM_getValue(key, null);
-	if(gotValue == null){
-		setGMCookie(key, defaultValue);
-		gotValue = defaultValue;
-	}
-	return gotValue;
-}
+aukroAlarm.startWorker = function () {
+    var self = this;
+    this.worker = later.setInterval(function () {
+        self.secondsToEnd = self.parseSecondsToEnd();
+        $('#secondsToEnd').text(self.secondsToEnd);
+        if (self.watching && self.secondsToEnd > 0 && self.secondsToEnd < self.secondsToEndAlarm) {
+            self.mAlarmSound.play();
+        }
+    }, this.sched);
+};
+
+aukroAlarm.stopWorker = function () {
+    if(typeof this.worker.clear === 'function') {
+        this.worker.clear();
+    }
+    delete this.worker;
+};
+
+// Init
+
+aukroAlarm.init = function () {
+    this.auctionId = window.location.href.split('.html')[0].slice(-10); // auction ID from page URL
+    if (!this.isInt(this.auctionId)) {
+        console.log('aukroAlarm: auctionId not found');
+        return;
+    }
+    //later.date.localTime();
+    this.sched = later.parse.text('every 1 sec');
+    if (this.sched.error > -1) {
+        console.log('aukroAlarm: later.parse failed');
+        return;
+    }
+    this.watching = this.getGMCookie(this.auctionId, false);
+    
+    $('div#siBidForm2').append('<div id="micaContainer" style="padding:20px;"><button id="micaButton"></button>&nbsp;&nbsp;&nbsp;Do konce: <span id="secondsToEnd">x</span> s, Alarm: '+this.secondsToEndAlarm+' s </div>');
+    $('#micaButton').text(this.watching ? 'Zrušit sledování' : 'Sledovat');
+    
+    this.mAlarmSound.addEventListener('ended', function() {
+        this.play();
+    });
+    this.startWorker();
+};
+
+//  Main
+
+$(document).ready(function() {
+    aukroAlarm.init();
+});
+
+//  Events
+
+$(document).on( 'click', 'button#micaButton', function() {
+    aukroAlarm.setGMCookie(aukroAlarm.auctionId, !aukroAlarm.watching);
+    location.reload();
+});
